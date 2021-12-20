8f380e2137c89c06190e48056af4b4d3cd09837a
Nicolò Vescera <nicolo.vescera@gmail.com>
2021-12-20 09:54:01 +0100

    Updated autoinstall
    Now the autoinstall install the pip requirements

--- autoinstall.py
+++ autoinstall.py
@@ -4,7 +4,7 @@ install_dir = "/opt/pytilingassistant"
 main_file = "main.py"
 local_bins = "/usr/local/bin"
 
-
+system("pip install -r requirements.txt")
 system(f"sudo mkdir {install_dir}")
 system(f"sudo cp {main_file} {install_dir}")
 system(f"sudo chmod +x {install_dir}/{main_file}")
